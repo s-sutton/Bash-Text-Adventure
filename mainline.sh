95687a18b615c9bc724befc5589d4fb446995427
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2020-02-07 07:08:05 -0600

    Update mainline.sh
    Added a short delay between lines for pacing.

--- mainline.sh
+++ mainline.sh
@@ -5,6 +5,7 @@ echo Your eyes sear and blur as you open them to the blinding light ahead of you
 sleep 3
 echo The intensity seems to fade, leaving you staring at the open sky. How long were you out?
 echo ""
+sleep 1
 echo "1. Too long."
 echo "2. I don't know."
 echo "3. Long enough to lose track of time."
