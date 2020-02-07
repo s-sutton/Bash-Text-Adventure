18f8dd8666bae0a0296438c9512e2e47bb995e6b
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2020-02-07 07:06:00 -0600

    Update mainline.sh
    Ah, duh. I don't need the slash if I put it in quotes. That should have been obvious, my bad.

--- mainline.sh
+++ mainline.sh
@@ -6,6 +6,6 @@ sleep 3
 echo The intensity seems to fade, leaving you staring at the open sky. How long were you out?
 echo ""
 echo "1. Too long."
-echo "2. I don\'t know."
+echo "2. I don't know."
 echo "3. Long enough to lose track of time."
 echo "4. I was asleep the exact lenght I meant to be. [Lie]"
