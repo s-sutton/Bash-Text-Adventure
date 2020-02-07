5bdbdd028861bd12d9e44921051774e9352df532
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2020-02-07 09:31:20 -0600

    Update mainline.sh
    Cleaned up the interface a bit and temporarily 0'd out the sleeps so I could test quicker

--- mainline.sh
+++ mainline.sh
@@ -1,17 +1,20 @@
+clear
 hostname
 date +%T:%N
 echo ""
 echo Your eyes sear and blur as you open them to the blinding light ahead of you. You blink a few times.
-sleep 3
+sleep 0
 echo The intensity seems to fade, leaving you staring at the open sky. How long were you out?
 echo ""
-sleep 1
+sleep 0
 echo "1. Too long."
 echo "2. I don't know."
 echo "3. Long enough to lose track of time."
 echo "4. I was asleep the exact lenght I meant to be. [Lie]"
 read -p "Your response:" option1
 if [[ "$option1" = "4" ]]
- then echo "I know you're lying, I can read the brackets."
+ then
+  clear
+  echo "I know you're lying, I can read the brackets."
  else read -p "Next." option2
 fi
