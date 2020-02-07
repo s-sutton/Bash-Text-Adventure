b5b96551fad886a8b5e796c6eb20ac3b6ca00e92
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2020-02-07 10:45:37 -0600

    Update roomstest.sh
    The list of functions I'm using clearly doesn't have the entire necessary syntax spelled out, but this does seem to be capable of doing what I'm looking for. This change just got rid of errors, the script doesn't work yet.

--- roomstest.sh
+++ roomstest.sh
@@ -1,11 +1,14 @@
-$room="beach"
+set $room="beach"
 function callroom {
- case room in
-  (beach) echo "You're at the beach"
+ case $room in
+  beach) echo "You're at the beach"
    sleep 2
    $room=hut
    callroom
-  (sea) echo "You're at sea."
-  (hut) echo "You're in a hut.
+   ;;
+  sea) echo "You're at sea."
+   ;;
+  hut) echo "You're in a hut."
+   ;;
 esac
 }
